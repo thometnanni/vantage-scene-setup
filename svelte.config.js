97251e34995e0b43353f4f9a5569659ded1103a3
Giacomo Nanni <gn.nanni@gmail.com>
2025-01-14 17:08:42 +0100

    vite not svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-const dev = process.argv.includes('dev');
 
-export default defineConfig({
-  base: dev ? '' : process.env.BASE_PATH,
-  plugins: [svelte()],
+export default {
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
   preprocess: vitePreprocess(),
-});
+}
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vantage-scene-setup',  // Ensure this is correct
   plugins: [svelte()],
 })
+
