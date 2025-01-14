b50d46310a8075f62cd2423026ce040af1000567
Giacomo Nanni <gn.nanni@gmail.com>
2025-01-14 14:18:21 +0100

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
+import { defineConfig } from 'vite';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+const dev = process.argv.includes('dev');
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
+export default defineConfig({
+  base: dev ? '' : process.env.BASE_PATH,
+  plugins: [svelte()],
   preprocess: vitePreprocess(),
-}
+});
